9225df130cf08f0b8705255e7d1821d25e0e9ccf
Ivaylo Palchev <ivopalchev@gmail.com>
2015-11-04 11:02:01 +0200

    Update ScorePoint.cs

--- ScorePoint.cs
+++ ScorePoint.cs
@@ -16,31 +16,31 @@ public class ScorePoint : MonoBehaviour {
 
 		if(Social.localUser.authenticated){
 
-			Social.ReportScore(ScoreScript.HighScore,"CgkIvLiw48EIEAIQAg", (bool success) => {
+			Social.ReportScore(ScoreScript.HighScore,"ID", (bool success) => {
 			});
 
 			if(ScoreScript.HighScore > 10) {
-				Social.ReportProgress("CgkIvLiw48EIEAIQAQ", 100.0f, (bool success) => {
+				Social.ReportProgress("ID", 100.0f, (bool success) => {
 				});
 			}
 			
 			if(ScoreScript.HighScore > 30) {
-				Social.ReportProgress("CgkIvLiw48EIEAIQAw", 100.0f, (bool success) => {
+				Social.ReportProgress("ID", 100.0f, (bool success) => {
 				});
 			}
 			
 			if(ScoreScript.HighScore > 50) {
-				Social.ReportProgress("CgkIvLiw48EIEAIQBQ", 100.0f, (bool success) => {
+				Social.ReportProgress("ID", 100.0f, (bool success) => {
 				});
 			}
 			
 			if(ScoreScript.HighScore > 100) {
-				Social.ReportProgress("CgkIvLiw48EIEAIQBA", 100.0f, (bool success) => {
+				Social.ReportProgress("ID", 100.0f, (bool success) => {
 				});
 			}
 			
 			if(ScoreScript.HighScore > 200) {
-				Social.ReportProgress("CgkIvLiw48EIEAIQBg", 100.0f, (bool success) => {
+				Social.ReportProgress("ID", 100.0f, (bool success) => {
 				});
 			}
 			
